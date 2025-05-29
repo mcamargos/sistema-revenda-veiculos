4febdfc7bd79e998cb97647147fc12d2dab0ff93
Matheus Camargos <camargosdantas@gmail.com>
2025-05-29 17:42:21 -0300

    Configurando o Backend e conectando DB

---
+++ sistema-backend/src/main/java/com/revendaveiculos/backend/SistemaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.revendaveiculos.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaBackendApplication.class, args);
+	}
+
+}
---
+++ sistema-backend/src/test/java/com/revendaveiculos/backend/SistemaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revendaveiculos.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
